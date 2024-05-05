065b23bb6e29553dc856694f51ce90b61903bb2a
Alex <alexbabits@gmail.com>
2024-05-05 17:15:34 -0400

    nanana, sounds

--- js/game.js
+++ js/game.js
@@ -18,7 +18,8 @@ const configurations = {
 const assets = {
     character: {
         hoa: 'hoa',
-        dikbutt: 'dikbutt'
+        dikbutt: 'dikbutt',
+        nanana: 'nanana'
     },
     obstacle: {
         pipe: {
@@ -36,11 +37,13 @@ const assets = {
         width: 144,
         background: {
             day: 'day',
-            night: 'night'
+            night: 'night',
+            final: 'final'
         },
         ground: 'ground',
         gameOver: 'game-over',
-        introMessage: 'intro-message'
+        introMessage: 'intro-message',
+        muteButton: 'mute-button'
     },
     scoreboard: {
         width: 25,
@@ -65,70 +68,93 @@ const assets = {
             dikbutt: {
                 go: 'dikbutt-go',
                 stop: 'dikbutt-stop'
+            }, 
+            nanana: {
+                go: 'nanana-go',
+                stop: 'nanana-stop'
             }
         }
     }
 }
 
-const game = new Phaser.Game(configurations)
+const game = new Phaser.Game(configurations);
 
-let gameOver
-let gameStarted
-let upButton
-let gameOverBanner 
-let introMessage 
-let player 
-let character
-let framesMoveUp 
-let backgroundDay
-let backgroundNight
-let ground
-let pipesGroup
-let gapsGroup
-let nextPipes 
-let currentPipe
-let scoreboardGroup 
-let score
-let inputDisabled
+let gameOver;
+let gameStarted;
+let spaceBar;
+let gameOverBanner;
+let introMessage; 
+let player; 
+let character;
+let framesMoveUp; 
+let backgroundDay;
+let backgroundNight;
+let backgroundFinal;
+let ground;
+let pipesGroup;
+let gapsGroup;
+let nextPipes; 
+let currentPipe;
+let scoreboardGroup; 
+let score;
+let muteButton;
 
 function preload() {
-    this.load.image(assets.scene.background.day, 'assets/background/day.png')
-    this.load.image(assets.scene.background.night, 'assets/background/night.png')
-    this.load.image(assets.scene.ground, 'assets/background/ground.png')
-    this.load.image(assets.obstacle.pipe.green.top, 'assets/background/pipe-green-top.png')
-    this.load.image(assets.obstacle.pipe.green.bottom, 'assets/background/pipe-green-bottom.png')
-    this.load.image(assets.obstacle.pipe.red.top, 'assets/background/pipe-red-top.png')
-    this.load.image(assets.obstacle.pipe.red.bottom, 'assets/background/pipe-red-bottom.png')
-    this.load.image(assets.scene.introMessage, 'assets/background/intro-message.png')
-    this.load.image(assets.scene.gameOver, 'assets/background/gameover.png')
+    this.load.image(assets.scene.background.day, 'assets/background/day.png');
+    this.load.image(assets.scene.background.night, 'assets/background/night.png');
+    this.load.image(assets.scene.background.final, 'assets/background/final.png');
+    this.load.image(assets.scene.ground, 'assets/background/ground.png');
+    this.load.image(assets.obstacle.pipe.green.top, 'assets/background/pipe-green-top.png');
+    this.load.image(assets.obstacle.pipe.green.bottom, 'assets/background/pipe-green-bottom.png');
+    this.load.image(assets.obstacle.pipe.red.top, 'assets/background/pipe-red-top.png');
+    this.load.image(assets.obstacle.pipe.red.bottom, 'assets/background/pipe-red-bottom.png');
+    this.load.image(assets.scene.introMessage, 'assets/background/intro-message.png');
+    this.load.image(assets.scene.gameOver, 'assets/background/gameover.png');
 
-    this.load.image(assets.scoreboard.number0, 'assets/numbers/number0.png')
-    this.load.image(assets.scoreboard.number1, 'assets/numbers/number1.png')
-    this.load.image(assets.scoreboard.number2, 'assets/numbers/number2.png')
-    this.load.image(assets.scoreboard.number3, 'assets/numbers/number3.png')
-    this.load.image(assets.scoreboard.number4, 'assets/numbers/number4.png')
-    this.load.image(assets.scoreboard.number5, 'assets/numbers/number5.png')
-    this.load.image(assets.scoreboard.number6, 'assets/numbers/number6.png')
-    this.load.image(assets.scoreboard.number7, 'assets/numbers/number7.png')
-    this.load.image(assets.scoreboard.number8, 'assets/numbers/number8.png')
-    this.load.image(assets.scoreboard.number9, 'assets/numbers/number9.png')
+    this.load.image(assets.scoreboard.number0, 'assets/numbers/number0.png');
+    this.load.image(assets.scoreboard.number1, 'assets/numbers/number1.png');
+    this.load.image(assets.scoreboard.number2, 'assets/numbers/number2.png');
+    this.load.image(assets.scoreboard.number3, 'assets/numbers/number3.png');
+    this.load.image(assets.scoreboard.number4, 'assets/numbers/number4.png');
+    this.load.image(assets.scoreboard.number5, 'assets/numbers/number5.png');
+    this.load.image(assets.scoreboard.number6, 'assets/numbers/number6.png');
+    this.load.image(assets.scoreboard.number7, 'assets/numbers/number7.png');
+    this.load.image(assets.scoreboard.number8, 'assets/numbers/number8.png');
+    this.load.image(assets.scoreboard.number9, 'assets/numbers/number9.png');
 
+    this.load.audio('flap', 'assets/audio/flap.mp3');
+    this.load.audio('die', 'assets/audio/die.mp3');
     this.load.audio('ding', 'assets/audio/ding.mp3');
+    this.load.audio('dikbutt', 'assets/audio/dikbutt.mp3');
+    this.load.audio('techno1', 'assets/audio/techno1.mp3');
+    this.load.audio('techno2', 'assets/audio/techno2.mp3');
+    this.load.audio('techno3', 'assets/audio/techno3.mp3');
+    this.load.audio('techno4', 'assets/audio/techno4.mp3');
+    this.load.audio('techno5', 'assets/audio/techno5.mp3');
 
-    this.load.spritesheet(assets.character.hoa, 'assets/characters/hoa.png', {frameWidth: 30, frameHeight: 29})
-    this.load.spritesheet(assets.character.dikbutt, 'assets/characters/dikbutt.png', {frameWidth: 25, frameHeight: 31})
+    this.load.image(assets.scene.muteButton, 'assets/background/mute-button.png');
+
+    this.load.spritesheet(assets.character.hoa, 'assets/characters/hoa.png', {frameWidth: 30, frameHeight: 29});
+    this.load.spritesheet(assets.character.dikbutt, 'assets/characters/dikbutt.png', {frameWidth: 25, frameHeight: 31});
+    this.load.spritesheet(assets.character.nanana, 'assets/characters/nanana.png', {frameWidth: 23, frameHeight: 29});
 }
 
 function create() {
-    backgroundDay = this.add.image(assets.scene.width, 256, assets.scene.background.day).setInteractive()
-    backgroundNight = this.add.image(assets.scene.width, 256, assets.scene.background.night).setInteractive()
-    backgroundDay.on('pointerdown', flapCharacter)
-    backgroundNight.on('pointerdown', flapCharacter)
+    backgroundDay = this.add.image(assets.scene.width, 256, assets.scene.background.day).setInteractive();
+    backgroundNight = this.add.image(assets.scene.width, 256, assets.scene.background.night).setInteractive();
+    backgroundFinal = this.add.image(assets.scene.width, 256, assets.scene.background.final).setInteractive();
+    backgroundDay.on('pointerdown', () => flapCharacter(this));
+    backgroundNight.on('pointerdown', () => flapCharacter(this));
+    backgroundFinal.on('pointerdown', () => flapCharacter(this));
     backgroundNight.visible = false
+    backgroundFinal.visible = false
 
-    gapsGroup = this.physics.add.group()
-    pipesGroup = this.physics.add.group()
-    scoreboardGroup = this.physics.add.staticGroup()
+    spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+    spaceBar.on('down', () => flapCharacter(this));
+
+    gapsGroup = this.physics.add.group();
+    pipesGroup = this.physics.add.group();
+    scoreboardGroup = this.physics.add.staticGroup();
 
     ground = this.physics.add.sprite(assets.scene.width, 458, assets.scene.ground)
     ground.setCollideWorldBounds(true)
@@ -138,7 +164,11 @@ function create() {
     introMessage.setDepth(30)
     introMessage.visible = false
 
-    upButton = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP)
+    this.muteButton = this.add.sprite(3, 490, 'mute-button').setOrigin(0, 0).setDepth(100).setInteractive();
+    this.muteButton.on('pointerup', function() {
+        this.sound.mute = !this.sound.mute;
+    }, this);
+    this.muteButton.visible = false;
 
     // HOA Animations
     this.anims.create({
@@ -154,7 +184,7 @@ function create() {
         key: assets.animation.character.hoa.stop,
         frames: [{
             key: assets.character.hoa,
-            frame: 1
+            frame: 0
         }],
         frameRate: 20
     })
@@ -178,9 +208,36 @@ function create() {
         frameRate: 20
     })
 
-    this.dingSound = this.sound.add('ding', {volume: 0.2});
+    // nanana Animations
+    this.anims.create({
+        key: assets.animation.character.nanana.go,
+        frames: this.anims.generateFrameNumbers(assets.character.nanana, {
+            start: 0,
+            end: 5
+        }),
+        frameRate: 6,
+        repeat: -1
+    })
+    this.anims.create({
+        key: assets.animation.character.nanana.stop,
+        frames: [{
+            key: assets.character.nanana,
+            frame: 1
+        }],
+        frameRate: 20
+    })
 
-    prepareGame(this)
+    this.dingSound = this.sound.add('ding', {volume: 0.05});
+    this.dieSound = this.sound.add('die', {volume: 0.2});
+    this.flapSound = this.sound.add('flap', {volume: 0.1});
+    this.dikbuttSound = this.sound.add('dikbutt', {volume: 0.1, loop: true});
+    this.techno1Sound = this.sound.add('techno1', {volume: 1, loop: true});
+    this.techno2Sound = this.sound.add('techno2', {volume: 0.5, loop: true});
+    this.techno3Sound = this.sound.add('techno3', {volume: 1, loop: true});
+    this.techno4Sound = this.sound.add('techno4', {volume: 0.5, loop: true});
+    this.techno5Sound = this.sound.add('techno5', {volume: 1, loop: true});
+
+    prepareGame(this);
 
     gameOverBanner = this.add.image(assets.scene.width, 206, assets.scene.gameOver)
     gameOverBanner.setDepth(20)
@@ -193,8 +250,6 @@ function update() {
 
     if (framesMoveUp > 0) {
         framesMoveUp--
-    } else if (Phaser.Input.Keyboard.JustDown(upButton)) {
-        flapCharacter()
     } else {
         player.setVelocityY(150) 
         if (player.angle < 90) player.angle += 2
@@ -219,8 +274,10 @@ function update() {
 
 // Player collision event. Game object that collided, in this case the character. 
 function playerDied(player) {
-    player.anims.play(getCharacterAnimation(character).stop)
     this.physics.pause()
+    player.anims.play(getCharacterAnimation(character).stop)
+    this.dieSound.play();
+    this.techno4Sound.stop();
     gameOver = true
     gameStarted = false
     gameOverBanner.visible = true
@@ -235,69 +292,89 @@ function updatePipes() {
 }
 
 function updateBackground() {
-    if (score % 10 == 0) {
-        backgroundDay.visible = !backgroundDay.visible
-        backgroundNight.visible = !backgroundNight.visible
-    } 
-    /*
-    // For future final background, after level 50
-    // Make it golden or something
-    else if (score > 49) {
+    if (score < 10) {
+        backgroundDay.visible = true
+        backgroundNight.visible = false
+        backgroundFinal.visible = false
+    } else if (score < 20) {
+        backgroundDay.visible = false
+        backgroundNight.visible = true
+        backgroundFinal.visible = false  
+    } else if (score < 30) {
+        backgroundDay.visible = true
+        backgroundNight.visible = false
+        backgroundFinal.visible = false  
+    } else if (score < 40) {
+        backgroundDay.visible = false
+        backgroundNight.visible = true
+        backgroundFinal.visible = false  
+    } else if (score < 50) {
+        backgroundDay.visible = true
+        backgroundNight.visible = false
+        backgroundFinal.visible = false 
+    } else {
         backgroundDay.visible = false
         backgroundNight.visible = false
         backgroundFinal.visible = true
-    }   
-    */
+    }
 }
 
 function updateScore(_, gap) {
     score++
     this.dingSound.play();
-    gap.destroy()
+    gap.destroy();
 
-    updatePipes()
-    updateBackground()
-    updateScoreboard()
+    updatePipes();
+    updateBackground();
+    updateScoreboard();
 }
 
 function makePipesAndGaps(scene) {
     if (!gameStarted || gameOver) return
 
-    const pipeTopY = Phaser.Math.Between(-120, 120)
-    pipesGroup.create(288, pipeTopY, currentPipe.top)
-    pipesGroup.create(288, pipeTopY + 420, currentPipe.bottom)
+    const pipeTopY = Phaser.Math.Between(-120, 120);
+    pipesGroup.create(288, pipeTopY, currentPipe.top);
+    pipesGroup.create(288, pipeTopY + 420, currentPipe.bottom);
 
-    const gap = scene.add.line(288, pipeTopY + 210, 0, 0, 0, 98)
-    gapsGroup.add(gap)
-    gap.visible = false
+    const gap = scene.add.line(288, pipeTopY + 210, 0, 0, 0, 98);
+    gapsGroup.add(gap);
+    gap.visible = false;
 }
 
-function flapCharacter() {
+function flapCharacter(scene) {
     if (gameOver) {
         restartGame();
         return;
     }
-    if (!gameStarted) startGame(game.scene.scenes[0]);
-
-    player.setVelocityY(-380)
-    player.angle = -20
-    framesMoveUp = 6
+    if (!gameStarted) {
+        startGame(game.scene.scenes[0]);
+        scene.techno4Sound.play();
+    }
+    //scene.flapSound.play(); // Removed for now, kinda annoying.
+    player.setVelocityY(-380);
+    player.angle = -20;
+    framesMoveUp = 6;
 }
 
+
 function getRandomCharacter() {
-    rng = Phaser.Math.Between(0, 1)
+    rng = Phaser.Math.Between(0, 2)
     if (rng == 0) {
         return assets.character.hoa;
-    } else {
+    } else if (rng == 1) {
         return assets.character.dikbutt;
+    } else {
+        return assets.character.nanana;
     }
 }
 
 function getCharacterAnimation(character) {
     if (character === assets.character.hoa) {
         return assets.animation.character.hoa;
-    } else {
+    } else if (character === assets.character.dikbutt) {
         return assets.animation.character.dikbutt;
+    } else {
+        return assets.animation.character.nanana;
     }
 }
 
@@ -320,16 +397,16 @@ function updateScoreboard() {
 
 // Restart the game, clear groups, hide game over objects and stop game physics.  
 function restartGame() {
-    pipesGroup.clear(true, true)
-    gapsGroup.clear(true, true)
-    scoreboardGroup.clear(true, true)
-    player.destroy()
-    gameOverBanner.visible = false
+    pipesGroup.clear(true, true);
+    gapsGroup.clear(true, true);
+    scoreboardGroup.clear(true, true);
+    player.destroy();
+    gameOverBanner.visible = false;
 
-    const gameScene = game.scene.scenes[0]
-    prepareGame(gameScene)
+    const gameScene = game.scene.scenes[0];
+    prepareGame(gameScene);
 
-    gameScene.physics.resume()
+    gameScene.physics.resume();
 }
 
 // Prepare game to be played
@@ -341,8 +418,9 @@ function prepareGame(scene) {
     gameOver = false
     backgroundDay.visible = true
     backgroundNight.visible = false
+    backgroundFinal.visible = false
     introMessage.visible = true
-
+    scene.muteButton.visible = true
     character = getRandomCharacter()
     player = scene.physics.add.sprite(55, 245, character)
     player.setScale(1.1);
@@ -357,7 +435,7 @@ function prepareGame(scene) {
 function startGame(scene) {
     gameStarted = true
     introMessage.visible = false
-
+    scene.muteButton.visible = false
     const score0 = scoreboardGroup.create(assets.scene.width, 30, assets.scoreboard.number0)
     score0.setDepth(20)
 
